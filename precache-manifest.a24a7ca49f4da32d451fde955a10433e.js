3407ef61f0c70da9cc39d4388cbe186ef8d6c8d9
Helder S�rvio <heldersrvio@gmail.com>
2020-07-14 18:10:10 -0300

    Updates

---
+++ precache-manifest.a24a7ca49f4da32d451fde955a10433e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72c69ce5bd11ad28515baf44ed3deb64",
+    "url": "/React-Restaurant-Page/index.html"
+  },
+  {
+    "revision": "22ed642424f1a6386920",
+    "url": "/React-Restaurant-Page/static/css/main.ae46256c.chunk.css"
+  },
+  {
+    "revision": "6ab0accb6ee85ea382ac",
+    "url": "/React-Restaurant-Page/static/js/2.3dcea801.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/React-Restaurant-Page/static/js/2.3dcea801.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22ed642424f1a6386920",
+    "url": "/React-Restaurant-Page/static/js/main.feed7305.chunk.js"
+  },
+  {
+    "revision": "67a2a2833dd6a446ad4c",
+    "url": "/React-Restaurant-Page/static/js/runtime-main.f16b68d9.js"
+  },
+  {
+    "revision": "7487e52a52d71ccc23e030c17807a8f9",
+    "url": "/React-Restaurant-Page/static/media/coffee-shop.7487e52a.jpg"
+  },
+  {
+    "revision": "92c6999f16b4d116ac7723ec273bbe50",
+    "url": "/React-Restaurant-Page/static/media/menu-template.92c6999f.jpg"
+  },
+  {
+    "revision": "5ba9acad827975c58b6e417979bb42cc",
+    "url": "/React-Restaurant-Page/static/media/wood-texture.5ba9acad.jpg"
+  },
+  {
+    "revision": "6f75282c4a47bf108ae7066e336ef891",
+    "url": "/React-Restaurant-Page/static/media/wood-texture2.6f75282c.jpg"
+  }
+]);
